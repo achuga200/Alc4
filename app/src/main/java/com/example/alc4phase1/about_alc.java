a22c69c63492eeb0e9a32969517c287c677a299a
achuga2oo <achugamonyeokwunna@gmail.com>
2019-07-13 12:59:47 +0100

    cert fixed

--- app/src/main/java/com/example/alc4phase1/about_alc.java
+++ app/src/main/java/com/example/alc4phase1/about_alc.java
@@ -1,15 +1,21 @@
 package com.example.alc4phase1;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.view.View;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.webkit.WebSettings;
 import android.os.Bundle;
+import android.view.View;
+import android.os.Handler;
+import android.webkit.SslErrorHandler;
+import android.net.http.SslError;
+
 
 public class about_alc extends AppCompatActivity {
 
      private WebView webView;
+     Handler handler;
+     View view;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,16 +24,15 @@ public class about_alc extends AppCompatActivity {
 
         webView = findViewById(R.id.webView);
 
-        webView.loadUrl("https://andela.com/alc/");
+        webView.loadUrl("https://www.andela.com/alc/");
         webView.setWebViewClient(new WebViewClient());
         WebSettings webSettings = webView.getSettings();
         webView.clearCache(true);
         webView.getSettings().setDomStorageEnabled(true);
         webSettings.setJavaScriptEnabled(true);
 
+        }
 
-
-    }
 
     /*public boolean shouldOverrideUrlLoading(WebView view, String url) {
         return super.shouldOverrideUrlLoading(view, url);
